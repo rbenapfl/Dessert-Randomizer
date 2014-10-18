5e484cd68abd1111508a2b24da6ca0ebd8931b6e
Robert Benapfl <rabsax@comcast.net>
2014-10-18 11:30:13 -0700

    add query to retrieve 16 random documents and send to client

--- source/routes/index.js
+++ source/routes/index.js
@@ -4,14 +4,19 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Dessert Randomizer!' });
-});
+})
 /* GET desserts */
 router.get('/desserts', function(req, res) {
   var db = req.db.desserts
-	db.find().skip(0).limit(2).toArray(function(err, result) {
-    if (err) throw err;
-    res.json(result);
-	});
-});
+  db.count({},function(err,count){
+  	if (err) throw err
+  	var skipMax = count-15
+  	var randomSkip = Math.floor((Math.random() * skipMax));
+		db.find().skip(randomSkip).limit(16).toArray(function(err, result) {
+    	if (err) throw err
+    	res.json(result)
+		})
+	})
+})
 
 module.exports = router;
