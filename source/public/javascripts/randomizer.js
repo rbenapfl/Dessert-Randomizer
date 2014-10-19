765f4f433c9e8c148ad4bca470c026f921d21755
Robert Benapfl <rabsax@comcast.net>
2014-10-19 10:53:12 -0700

    renamed routes file, delete users path, add MVC architecture for client Javascript

--- source/public/javascripts/randomizer.js
+++ source/public/javascripts/randomizer.js
@@ -1,15 +1,38 @@
 window.onload = function(){ 
-	DessertRandomizer.addRandomizerListener()
+	var controller = new DessertController(new DessertView, new DessertModel)
+	controller.init()
 }
 
-DessertRandomizer = {
-	addRandomizerListener: function(){
+
+function DessertController(dessertView,dessertModel) {
+	this.display = dessertView
+	this.model = dessertModel
+}
+
+DessertController.prototype = {
+	init: function() {
 		var randomizeButton = document.getElementById('randomizer')
-		randomizeButton.addEventListener("click", DessertRandomizer.requestDesserts)
+		randomizeButton.addEventListener("click", this.requestDesserts.bind(this))
 	},
-	requestDesserts: function(){
+	requestDesserts: function() {
 		$.ajax({type: 'GET',url: '/desserts',success: function(dessertJson){
 			console.log(dessertJson)
 		}})
 	}
+}
+
+function DessertModel() {
+	this.desserts = []
+}
+
+DessertModel.prototype = {
+
+}
+
+function DessertView() {
+
+}
+
+DessertView.prototype = {
+
 }
